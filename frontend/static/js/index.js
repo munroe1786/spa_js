1cb1e12dab5f2b32cd6c9f9f01315d5b0823e38d
munroe1786 <era3munroe@gmail.com>
2021-06-27 10:28:25 -0500

    add router and routes to index.js

---
+++ frontend/static/js/index.js
@@ -0,0 +1,10 @@
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing Dashboard") },
+        { path: "/posts", view: () => console.log("Viewing Posts") },
+        { path: "/settings", view: () => console.log("Viewing Settings") }, 
+    ];
+
+    // Test each route for potential match
+    const potentialMatches = routes.map()
+};
