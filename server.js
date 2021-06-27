748b3a6582e084be0f6bfbb2902ad10d7ffd3692
munroe1786 <era3munroe@gmail.com>
2021-06-27 10:24:13 -0500

    add app functions to server.js and add index.js file

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const path = require("path");
 
-const app = 
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.listen(process.env.PORT || 9292, () => console.log("Server running..."));
