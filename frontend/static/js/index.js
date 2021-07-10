e61edfbc815b8636591adbae7465b0a23bcebfee
munroe1786 <era3munroe@gmail.com>
2021-07-09 19:21:17 -0500

    add Object.fromEntries return in getParams method

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -8,9 +8,9 @@ const getParams = match => {
     const values = match.result.slice(1);
     const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
 
-    console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)));
-
-    return {};
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
 };
 
 const navigateTo = url => {
